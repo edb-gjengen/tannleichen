e96e0d712aeaafc997fbf10b7888506c41dab6ce
Dan-Mikkel <danmikkh@ulrik.uio.no>
2017-02-24 22:00:25 +0100

    Renamed test to follow set standard

--- test_alphabet.py
+++ test_alphabet.py
@@ -4,7 +4,10 @@ rows = 11
 
 text = raw_input('Enter text:\n')
 c = map(lambda x: ord(x)-ord('a'),text)
+s = ""
 for i in range(rows):
     for j in c:
-        print alphabet[j][i],
-    print ""
+        s+= alphabet[j][i]
+    s+= "\n"
+
+print s
